40c4fb15b4128facab1c2e3b107ec5da436a07a0
purbayan1105 <purbayan.kol@gmail.com>
2024-08-06 09:26:04 +0530

    react visibility sensor installed

--- src/components/MyskillArray.tsx
+++ src/components/MyskillArray.tsx
@@ -1,52 +1,85 @@
+import React from "react";
 import CountUp from "react-countup";
+import ReactVisibilitySensor from "react-visibility-sensor";
 
-const MyskillArray = () => {
+const MyskillArray: React.FC = () => {
   return (
-    <>
-      <div className="bg-white  dark:bg-[#111934] poppins pt-8 pb-8">
-        <div className=" pt-5 flex justify-center items-center text-4xl font-semibold">
-          Journey So Far
+    <div className="bg-whitepn dark:bg-[#111934] poppins pt-8 pb-8">
+      <div className="pt-5 flex justify-center items-center text-4xl font-semibold">
+        Journey So Far
+      </div>
+      <div className="grid lg:grid-cols-4 lg:h-[20dvh] justify-center items-center px-5 lg:px-16 space-y-8 py-8 md:py-0">
+        <div className="flex gap-5 items-center lg:pt-5">
+          <ReactVisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }: { isVisible: boolean }) => (
+              <>
+                <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent">
+                  {isVisible ? <CountUp start={0} end={1} duration={2} /> : "0"}
+                  +
+                </div>
+                <div>
+                  <div className="text-gray-500">Years Of </div>
+                  <div className="text-gray-500">Experience </div>
+                </div>
+              </>
+            )}
+          </ReactVisibilitySensor>
         </div>
-        <div className=" grid  lg:grid-cols-4 lg:h-[20dvh] justify-center items-center px-5 lg:px-16 space-y-8 py-8 md:py-0">
-          <div className="flex gap-5 items-center lg:pt-5">
-            <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent ">
-              <CountUp start={0} end={1}></CountUp>+
-            </div>
-            <div className="">
-              <div className="text-gray-500">Years Of </div>
-              <div className="text-gray-500">Experience </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent ">
-              <CountUp start={0} end={10} duration={5} suffix="+"></CountUp>
-            </div>
-            <div className="">
-              <div className="text-gray-500">Technologies </div>
-              <div className="text-gray-500">Mastered </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent ">
-              <CountUp start={0} end={5} duration={2} suffix=""></CountUp>
-            </div>
-            <div className="">
-              <div className="text-gray-500">projects</div>
-              <div className="text-gray-500">Contribution </div>
-            </div>
-          </div>
-          <div className="flex items-center gap-5">
-            <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent ">
-              <CountUp start={0} end={100} duration={2} suffix="%"></CountUp>
-            </div>
-            <div className="">
-              <div className="text-gray-500">Client </div>
-              <div className="text-gray-500">Satisfaction </div>
-            </div>
-          </div>
+        <div className="flex items-center gap-5">
+          <ReactVisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }: { isVisible: boolean }) => (
+              <>
+                <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent">
+                  {isVisible ? (
+                    <CountUp start={0} end={10} duration={5} suffix="+" />
+                  ) : (
+                    "0+"
+                  )}
+                </div>
+                <div>
+                  <div className="text-gray-500">Technologies </div>
+                  <div className="text-gray-500">Mastered </div>
+                </div>
+              </>
+            )}
+          </ReactVisibilitySensor>
+        </div>
+        <div className="flex items-center gap-5">
+          <ReactVisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }: { isVisible: boolean }) => (
+              <>
+                <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent">
+                  {isVisible ? <CountUp start={0} end={5} duration={2} /> : "0"}
+                </div>
+                <div>
+                  <div className="text-gray-500">Projects</div>
+                  <div className="text-gray-500">Contribution </div>
+                </div>
+              </>
+            )}
+          </ReactVisibilitySensor>
+        </div>
+        <div className="flex items-center gap-5">
+          <ReactVisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+            {({ isVisible }: { isVisible: boolean }) => (
+              <>
+                <div className="text-6xl font-bold text-gray-900 dark:bg-gradient-to-tr from-gray-400 to-gray-100 dark:bg-clip-text w-fit dark:text-transparent">
+                  {isVisible ? (
+                    <CountUp start={0} end={100} duration={2} suffix="%" />
+                  ) : (
+                    "0%"
+                  )}
+                </div>
+                <div>
+                  <div className="text-gray-500">Client </div>
+                  <div className="text-gray-500">Satisfaction </div>
+                </div>
+              </>
+            )}
+          </ReactVisibilitySensor>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
